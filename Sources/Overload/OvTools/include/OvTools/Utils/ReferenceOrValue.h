04494a50fc60d4ac70c2c36478b9b07fd78e10b4
Adrien GIVRY <adrien@givry.pro>
2019-08-17 11:18:01 -0400

    ReferenceOrValue class implemented
    The ReferenceOrValue class is a good way to create a wrapper for a value
    or a reference. It is usefull in contexts like UI, it prevents creating
    events and listener to keep, for example, a UI widget up-to-date with a
    component value. Instead, we can use some ReferenceOrValue in our UI widgets
    and in the editor we can assign a reference to component values to them.

---
+++ Sources/Overload/OvTools/include/OvTools/Utils/ReferenceOrValue.h
@@ -0,0 +1,101 @@
+/**
+* @project: Overload
+* @author: Overload Tech.
+* @restrictions: This software may not be resold, redistributed or otherwise conveyed to a third party.
+*/
+
+#pragma once
+
+#include <variant>
+
+namespace OvTools::Utils
+{
+	/**
+	* A simple class that can represent a reference or a value of the given type.
+	* The usage of the data is the same for both usages.
+	* The goal is to be able to instanciate objects that can be a reference or a value without
+	* taking care of what it is.
+	*/
+	template <typename T>
+	class ReferenceOrValue
+	{
+	public:
+		/**
+		* Construct the ReferenceOrValue instance with a reference
+		* @param p_reference
+		*/
+		ReferenceOrValue(std::reference_wrapper<T> p_reference) : m_data{ &p_reference.get() }
+		{
+		}
+
+		/**
+		* Construct the ReferenceOrValue instance with a value
+		* @param p_value
+		*/
+		ReferenceOrValue(T p_value = T()) : m_data{ p_value }
+		{
+		}
+
+		/**
+		* Make the ReferenceOrValue a reference
+		* @param p_reference
+		*/
+		void MakeReference(T& p_reference)
+		{
+			m_data = &p_reference;
+		}
+
+		/**
+		* Make the ReferenceOrValue a value
+		* @param p_value
+		*/
+		void MakeValue(T p_value = T())
+		{
+			m_data = p_value;
+		}
+
+		/**
+		* Implicit conversion of a ReferenceOrValue to a T
+		*/
+		operator T()
+		{
+			return Get();
+		}
+
+		/**
+		* Assignment operator thats call the setter of the ReferenceOrValue instance
+		* @param p_value
+		*/
+		ReferenceOrValue<T>& operator=(T p_value)
+		{
+			Set(p_value);
+			return *this;
+		}
+
+		/**
+		* Returns the value (From reference or directly from the value)
+		*/
+		T Get() const
+		{
+			if (auto pval = std::get_if<T>(&m_data))
+				return *pval;
+			else
+				return *std::get<T*>(m_data);
+		}
+
+		/**
+		* Sets the value (To the reference or directly to the value)
+		* @param p_value
+		*/
+		void Set(T p_value)
+		{
+			if (auto pval = std::get_if<T>(&m_data))
+				* pval = p_value;
+			else
+				*std::get<T*>(m_data) = p_value;
+		}
+
+	private:
+		std::variant<T, T*> m_data;
+	};
+}
