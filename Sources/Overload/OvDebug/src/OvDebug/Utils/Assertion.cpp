1eaa7117278b6ea8c1a43d7b5d185d590c6e365f
Adrien GIVRY <adrien@givry.pro>
2020-01-07 19:02:50 -0500

    Licence update to MIT

--- Sources/Overload/OvDebug/src/OvDebug/Utils/Assertion.cpp
+++ Sources/Overload/OvDebug/src/OvDebug/Utils/Assertion.cpp
@@ -1,7 +1,7 @@
 /**
 * @project: Overload
 * @author: Overload Tech.
-* @restrictions: This software may not be resold, redistributed or otherwise conveyed to a third party.
+* @licence: MIT
 */
 
 #include "OvDebug/Utils/Assertion.h"
