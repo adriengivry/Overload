990ba2a2a3ddb6aaa39afb0d54de7a9c5afe6537
Adrien GIVRY <adrien@givry.pro>
2020-10-17 20:24:20 -0400

    Revert "Refactor service locator"

--- Sources/Overload/OvDebug/include/OvDebug/Utils/Assertion.h
+++ Sources/Overload/OvDebug/include/OvDebug/Utils/Assertion.h
@@ -6,6 +6,31 @@
 
 #pragma once
 
-#include <cassert>
+#include <string>
 
-#define OVASSERT(condition, message) assert((condition) && message)
+#include "OvDebug/API/Export.h"
+
+#define OVASSERT(condition, message) OvDebug::Assertion::Assert(condition, message)
+
+namespace OvDebug
+{
+	/**
+	* Wrapper for C++ assert
+	*/
+	class API_OVDEBUG Assertion
+	{
+	public:
+
+		/**
+		* Disabled constructor
+		*/
+		Assertion() = delete;
+
+		/**
+		* C++ assertion wrapped call
+		* @param p_condition
+		* @param p_message
+		*/
+		static void Assert(bool p_condition, const std::string& p_message = "");
+	};
+}
---
+++ Sources/Overload/OvDebug/src/OvDebug/Utils/Assertion.cpp
@@ -0,0 +1,14 @@
+/**
+* @project: Overload
+* @author: Overload Tech.
+* @licence: MIT
+*/
+
+#include "OvDebug/Utils/Assertion.h"
+
+#include <assert.h>
+
+void OvDebug::Assertion::Assert(bool p_condition, const std::string& p_message)
+{
+	assert(p_condition && p_message.c_str());
+}
