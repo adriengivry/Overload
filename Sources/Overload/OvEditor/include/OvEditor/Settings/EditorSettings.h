548a302ed715fb16c752f9794fcf702f2f6816d8
sameep-git <sameep.shah@tcu.edu>
2025-02-07 21:17:29 -0600

    enabled frustum culling everywhere
    Signed-off-by: sameep-git <sameep.shah@tcu.edu>

--- Sources/Overload/OvEditor/include/OvEditor/Settings/EditorSettings.h
+++ Sources/Overload/OvEditor/include/OvEditor/Settings/EditorSettings.h
@@ -75,9 +75,9 @@ namespace OvEditor::Settings
 
 		inline static Property<bool> ShowGeometryBounds = { false };
 		inline static Property<bool> ShowLightBounds = { false };
-		inline static Property<bool> EditorFrustumGeometryCulling = { false };
-		inline static Property<bool> EditorFrustumLightCulling = { false };
-		inline static Property<bool> DebugFrustumCulling = { false };
+		inline static Property<bool> EditorFrustumGeometryCulling = { true };
+		inline static Property<bool> EditorFrustumLightCulling = { true };
+		inline static Property<bool> DebugFrustumCulling = { true };
 		inline static Property<float> LightBillboardScale = { 0.5f };
 		inline static Property<float> TranslationSnapUnit = { 1.0f };
 		inline static Property<float> RotationSnapUnit = { 15.0f };
