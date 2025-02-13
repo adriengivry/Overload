c9030b50ce0c389573d14be0207a057339a61c53
Adrien GIVRY <contact@adrien-givry.com>
2025-02-13 13:14:50 -0500

    Added utility scritps

---
+++ Scripts/GenerateProjects.bat
@@ -0,0 +1,9 @@
+@echo off
+SET version=%~1%
+if "%~1"=="" SET version="vs2022"
+
+SET premake_path=%~dp0\..\Dependencies\premake5\bin\premake5.exe
+
+pushd %~dp0\..\Sources\Overload
+call %premake_path% %version%
+popd
