68b906008af76feeae3fe427b179cdb45084a1a3
Sameep Shah <129002633+sameep-git@users.noreply.github.com>
2025-02-13 21:59:32 -0600

    Added GetPathToAppdata util function and removed getenv() usage (#360)

--- Sources/Overload/OvTools/src/OvTools/Utils/SystemCalls.cpp
+++ Sources/Overload/OvTools/src/OvTools/Utils/SystemCalls.cpp
@@ -8,6 +8,9 @@
 #include "OvTools/Utils/SystemCalls.h"
 
 #include <Windows.h>
+#include <ShlObj.h>
+#include <memory>
+#include <assert.h>
 
 void OvTools::Utils::SystemCalls::ShowInExplorer(const std::string & p_path)
 {
@@ -29,4 +32,20 @@ void OvTools::Utils::SystemCalls::EditFile(const std::string & p_file)
 void OvTools::Utils::SystemCalls::OpenURL(const std::string& p_url)
 {
 	ShellExecute(0, 0, p_url.c_str(), 0, 0, SW_SHOW);
+}
+
+std::string OvTools::Utils::SystemCalls::GetPathToAppdata()
+{
+	// Retrieve app-data path
+	PWSTR rawPath = nullptr;
+	const HRESULT hr = SHGetKnownFolderPath(FOLDERID_RoamingAppData, 0, nullptr, &rawPath);
+	std::unique_ptr<wchar_t, decltype(&CoTaskMemFree)> path(rawPath, CoTaskMemFree);
+	assert(SUCCEEDED(hr) && "Failed to get AppData path");
+	
+	// Convert app-data path from wide char to UTF-8 string
+	const int size_needed = WideCharToMultiByte(CP_UTF8, 0, path.get(), -1, nullptr, 0, nullptr, nullptr);
+	assert(size_needed > 0 && "failed to convert from wide char to UTF-8");
+	std::string appDataPath(size_needed, 0);
+	WideCharToMultiByte(CP_UTF8, 0, path.get(), -1, &appDataPath[0], size_needed, nullptr, nullptr);
+	return appDataPath;
 }
