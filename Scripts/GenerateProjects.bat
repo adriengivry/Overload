19ca796ab484754d8a88cfec054d49963441e6a4
Adrien GIVRY <contact@adrien-givry.com>
2025-02-14 12:54:33 -0500

    Added support for spaces in path

--- Scripts/GenerateProjects.bat
+++ Scripts/GenerateProjects.bat
@@ -2,8 +2,8 @@
 SET version=%~1%
 if "%~1"=="" SET version="vs2022"
 
-SET premake_path=%~dp0\..\Dependencies\premake5\bin\premake5.exe
+SET premake_path="%~dp0\..\Dependencies\premake5\bin\premake5.exe"
 
-pushd %~dp0\..\Sources\Overload
+pushd "%~dp0\..\Sources\Overload"
 call %premake_path% %version%
 popd
